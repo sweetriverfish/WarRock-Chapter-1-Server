26818ccb1ce0211f13a06b9eb60e5ff00cda4950
Artem <artemx100@gmail.com>
2025-01-22 22:48:22 +0200

    migrate to net 8

--- Authorization/Enums/Packets.cs
+++ Authorization/Enums/Packets.cs
@@ -1,5 +1,7 @@
 ﻿namespace Authorization.Enums {
-    public enum Packets : ushort {
+    public enum Packets : ushort
+    {
+        Connection = 0x1200,
         ServerList = 0x1100,
         Nickname = 0x1101,
         Launcher = 0x1010
